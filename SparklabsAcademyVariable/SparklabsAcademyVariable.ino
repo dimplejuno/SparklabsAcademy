78649c83197d4c9c62a60ebe8b392f8673468a10
Steve Park <dimplejuno@Steveui-MacBook-Pro.local>
2017-09-05 16:06:06 +0900

    variable

---
+++ SparklabsAcademyVariable/SparklabsAcademyVariable.ino
@@ -0,0 +1,11 @@
+int americano = 2000;
+int latte = 3000;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("americano + latte = ");
+  Serial.println(americano + latte);
+}
+
+void loop() {
+}
